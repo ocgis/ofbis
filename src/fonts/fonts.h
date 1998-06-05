8261c19a6b7787fd2e826c5226be2f2b8087f361
tomas <tomas>
1998-06-05 20:36:02 +0000

    Put built-in font declarations in libfb.h instead of a separate include file.

--- src/fonts/fonts.h
+++
@@ -1,8 +0,0 @@
-extern unsigned char fontdata_6x11[];
-extern int fontwidth_6x11, fontheight_6x11;
-
-extern unsigned char fontdata_8x8[];
-extern int fontwidth_8x8, fontheight_8x8;
-
-extern unsigned char fontdata_8x16[];
-extern int fontwidth_8x16, fontheight_8x16;
--- src/libfb.h
+++ src/libfb.h
@@ -144,6 +144,17 @@ void	FBfreebltpblk(FBBLTPBLK *fbb);
 
 void	FBsetfont(FB *f, FBFONT *newfont);
 
+/* built-in font declarations */
+
+extern unsigned char fontdata_6x11[];
+extern int fontwidth_6x11, fontheight_6x11;
+
+extern unsigned char fontdata_8x8[];
+extern int fontwidth_8x8, fontheight_8x8;
+
+extern unsigned char fontdata_8x16[];
+extern int fontwidth_8x16, fontheight_8x16;
+
 /* debug functions */
 
 void	FBfinfdump(FB *f);
