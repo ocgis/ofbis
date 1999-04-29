03f93cbc75944abce6be90da9b5803558c382c39
tomas <tomas>
1999-04-29 21:46:47 +0000

    Moved functions into fbmmap.c and declarations in fbmmap.h

---
+++ src/fbmmap.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "ofbis.h"
+#include "fberror.h"
+
+void
+FBmap(FB *f)
+{
+  off_t offset = 0;
+
+  /* Some cards need a special mmap with offset */
+
+  if(f->finf.accel == FB_ACCEL_SUN_CGSIX)
+    offset = 0x70000000 + 0x16000;  
+
+	/* Map fb into memory */
+
+	if ( (f->sbuf=(unsigned short *)mmap((void *)0,f->finf.smem_len, 
+					     PROT_READ | PROT_WRITE,
+					     MAP_SHARED, f->fb, offset)) == (void *)-1 )
+	{
+		FBerror( FATAL | SYSERR, "FBopen: fb mmap failed" );
+	}
+}
+
+void
+FBunmap(FB *f)
+{
+	/* Unmap framebuffer from memory */
+
+	if ( ( f->visible ) && ( munmap ( (void *)f->sbuf, f->finf.smem_len ) == -1 ) )
+	{
+		FBerror( WARNING | SYSERR, "FBclose: munmap failed");
+	}
+}
+
+
+
+
--- src/fbmmap.h
+++ src/fbmmap.h
@@ -1,32 +1,11 @@
 #ifndef __FBMMAP_H_
 #define __FBMMAP_H_
 
-#include <unistd.h>
-#include <sys/mman.h>
-#include "ofbis.h"
-
-inline static void
-FBmap(FB *f)
-{
-	/* Map fb into memory */
-
-	if ( (f->sbuf=(unsigned short *)mmap((void *)0,f->finf.smem_len, 
-					     PROT_READ | PROT_WRITE,
-					     MAP_SHARED, f->fb, (off_t)0)) == (void *)-1 )
-	{
-		FBerror( FATAL | SYSERR, "FBopen: fb mmap failed" );
-	}
-}
-
-inline static void
-FBunmap(FB *f)
-{
-	/* Unmap framebuffer from memory */
-
-	if ( ( f->visible ) && ( munmap ( (void *)f->sbuf, f->finf.smem_len ) == -1 ) )
-	{
-		FBerror( WARNING | SYSERR, "FBclose: munmap failed");
-	}
-}
+void FBmap(FB *f);
+void FBunmap(FB *f);
 
 #endif
+
+
+
+
