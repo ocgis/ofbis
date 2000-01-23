d903c0cfd77e7316442d180ea6c53921082d22fb
tomas <tomas>
2000-01-23 22:47:52 +0000

    * Keep an internal palette, which is used by FBc24_to_cnative() and   FBcnative_to_c24().

--- src/fbcmap.c
+++ src/fbcmap.c
@@ -10,6 +10,7 @@ FBgetcmap( FB *f )
 	static int	length=0;	/* length of colourmap */
 	struct fb_cmap	cmap;
 	FBCMAP		*fbcmap;	/* new colourmap */
+	int i;
 
 	fbcmap = (FBCMAP *) FBalloc( sizeof(FBCMAP) );
 
@@ -23,6 +24,7 @@ FBgetcmap( FB *f )
 		length = 0;
 	}
 	if ( length == 0 )*/
+
 	{
 	length = 1<<f->vinf.bits_per_pixel;
 	cmap.red=(unsigned short *) FBalloc( length * sizeof(unsigned short) );
@@ -46,7 +48,16 @@ FBgetcmap( FB *f )
 	fbcmap->red=cmap.red;
 	fbcmap->green=cmap.green;
 	fbcmap->blue=cmap.blue;
-	f->cmap=fbcmap;
+
+	/* Update the internal palette. */
+	f->cmap->start = fbcmap->start;
+	f->cmap->end = fbcmap->end;
+	for(i = fbcmap->start ; i <= fbcmap->end ; i++) {
+	  f->cmap->red[i] = fbcmap->red[i];
+	  f->cmap->green[i] = fbcmap->green[i];
+	  f->cmap->blue[i] = fbcmap->blue[i];
+	}
+
 	return fbcmap;
 }
 
@@ -54,6 +65,7 @@ void
 FBputcmap( FB *f, FBCMAP *fbcmap )
 {
 	struct fb_cmap	cmap;
+	int i;
 
 	cmap.start=fbcmap->start;
 	cmap.len=fbcmap->end-fbcmap->start+1;
@@ -65,7 +77,13 @@ FBputcmap( FB *f, FBCMAP *fbcmap )
 	{
 		FBerror( FATAL | SYSERR, "FBputcmap: Put colourmap failed" );
 	}
-	f->cmap=fbcmap;
+
+	/* Update the internal palette too. */
+	for(i = fbcmap->start ; i <= fbcmap->end ; i++) {
+	  f->cmap->red[i] = fbcmap->red[i];
+	  f->cmap->green[i] = fbcmap->green[i];
+	  f->cmap->blue[i] = fbcmap->blue[i];
+	}
 }
 
 void
@@ -82,7 +100,6 @@ FBc24_to_cnative( FB *f, u_int32_t col24 )
 {
   int nr_of_colours, best_diff, best_match, i, diff;
   int red, green, blue;
-  FBCMAP *fbcmap;
   u_int32_t pval;
 
   /* For palette based modes, we try to find the best matching
@@ -90,24 +107,19 @@ FBc24_to_cnative( FB *f, u_int32_t col24 )
    */
   if(f->finf.visual == FB_VISUAL_PSEUDOCOLOR) {
     nr_of_colours = 1 << f->vinf.bits_per_pixel;
-    fbcmap = FBgetcmap(f);
-    if(fbcmap == NULL)
-      return 0;
 
     best_diff = 1024;
     best_match = nr_of_colours - 1;
     for(i = 0 ; i < nr_of_colours ; i++) {
-      red   = ((col24 >> 16) & 0xff) - (fbcmap->red[i] >> 8);
-      green = ((col24 >>  8) & 0xff) - (fbcmap->green[i] >> 8);
-      blue  = ((col24      ) & 0xff) - (fbcmap->blue[i] >> 8);
+      red   = ((col24 >> 16) & 0xff) - (f->cmap->red[i] >> 8);
+      green = ((col24 >>  8) & 0xff) - (f->cmap->green[i] >> 8);
+      blue  = ((col24      ) & 0xff) - (f->cmap->blue[i] >> 8);
       diff = red * red + green * green + blue * blue;
       if(diff < best_diff) {
 	best_diff = diff;
 	best_match = i;
       }
     }
-
-    FBfreecmap(fbcmap);
 
     return best_match;
   }
@@ -122,22 +134,17 @@ FBc24_to_cnative( FB *f, u_int32_t col24 )
 u_int32_t
 FBcnative_to_c24( FB *f, u_int32_t col )
 {
-  FBCMAP *fbcmap;
   int index, nr_of_colours;
   u_int32_t colour;
 
   /* This picks out the colour from the palette. */
   if(f->finf.visual == FB_VISUAL_PSEUDOCOLOR) {
-    fbcmap = FBgetcmap(f);
-    if(fbcmap == NULL)
-      return 0;
     nr_of_colours = 1 << f->vinf.bits_per_pixel;
     index = col < nr_of_colours ? col : nr_of_colours-1;
     index = index >= 0 ? index : 0;
-    colour = ((((fbcmap->red[index] >> 8) & 0xff) << 16) |
-	      (((fbcmap->green[index] >> 8) & 0xff) <<  8) |
-	      (((fbcmap->blue[index] >> 8) & 0xff) <<  0));
-    FBfreecmap(fbcmap);
+    colour = ((((f->cmap->red[index] >> 8) & 0xff) << 16) |
+	      (((f->cmap->green[index] >> 8) & 0xff) <<  8) |
+	      (((f->cmap->blue[index] >> 8) & 0xff) <<  0));
     return colour;
   }
 
